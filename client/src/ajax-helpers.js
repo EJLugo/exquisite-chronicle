b91596f21b592732265db3c536af74a72fbd27db
Curt Morgan <curtmorgan@ool-ad0205a4.dyn.optonline.net>
2018-12-10 15:25:17 -0500

    all GET routes for front end ajax

--- client/src/ajax-helpers.js
+++ client/src/ajax-helpers.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'http://localhost:3001';
 
 //GET Request
 async function getUser(user_id){
@@ -12,9 +12,55 @@ async function getUser(user_id){
 	}
 };
 
-async function getCompletedStories(){
+async function allCompletedStories(){
 	try{
 		const allCompletedStories = await axios.get(`${BASE_URL}/stories`);
+		return allCompletedStories.data;
+	}catch(e){
+		console.log(e);
+	}
+};
+
+async function oneCompletedStory(story_id){
+	try{
+		const oneCompletedStory = await axios.get(`${BASE_URL}/stories/${story_id}`);
+		return oneCompletedStory.data;
+	}catch(e){
+		console.log(e);
+	}
+}
+
+async function allPrompts(){
+	try{
+		const allPrompts = await axios.get(`${BASE_URL}/prompts`);
+		return allPrompts.data;
+	}catch(e){
+		console.log(e);
+	}
+};
+
+async function allUserPrompts(user_id){
+	try{
+		const userPrompt = await axios.get(`${BASE_URL}/user/${user_id}/prompts`);
+		return userPrompt.data;
+	}catch(e){
+		console.log(e);
+	}
+};
+
+async function allUserChapters(user_id){
+	try{
+		const userChapter = await axios.get(`${BASE_URL}/user/${user_id}/chapters`);
+		return userChapter.data;
+	}catch(e){
+		console.log(e);
+	}
+};
+
+async function oneUserChapter(user_id, chapter_id){
+	try{
+		const userChapter = await axios.get(`${BASE_URL}/user/${user_id}/chapters/${chapter_id}`);
+		return userChapter.data;
 	}catch(e){
 		console.log(e);
 	}
@@ -22,5 +68,10 @@ async function getCompletedStories(){
 
 module.exports ={
 	getUser,
-	getCompletedStories,
+	allCompletedStories,
+	oneCompletedStory,
+	allPrompts,
+	allUserPrompts,
+	allUserChapters,
+	oneUserChapter,
 };
