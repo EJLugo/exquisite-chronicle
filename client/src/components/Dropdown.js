e4a30a693fb3f1280d1948ed4789690301df1b34
Student_11 <jhonymaurad@gmail.com>
2018-12-11 09:12:15 -0500

    drowpdown component added

---
+++ client/src/components/Dropdown.js
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+
+export default class Dropdown extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      storyGenres: ['Tragedy','Science fiction','Fantasy','Mythology','Adventure','Mystery,','Drama','Romance','Action / Adventure','Horror']
+    };
+this.handleChange = this.handleChange.bind(this);
+  }
+
+handleChange(e){
+    this.setState({
+      value: e.target.value,
+    });
+}
+
+  render(){
+    return(
+      <div>
+        <div >
+          <label >
+            Select a story genre:
+            <select className='dropdownList' value={this.state.value} onChange={this.handleChange}>
+              {
+                this.state.storyGenres.map(genre =>(
+                  <option key={genre.id}
+                          value={genre}
+                    >{genre}</option>
+                ))
+              }
+            </select>
+          </label>
+        </div>
+    </div>
+    )
+  }
+}
