c4abb2eee5cca7c3399a13fc7c04c2ce74f83167
Curt Morgan <curtmorgan@ool-ad0205a4.dyn.optonline.net>
2018-12-11 16:09:31 -0500

    stash changes

--- test.js
+++ test.js
@@ -6,14 +6,20 @@ const userData = {
 	birthday: '29 June 1998',
 };
 
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwidXNlcm5hbWUiOiJjYzEyOSIsImlhdCI6MTU0NDU2MDg0OH0.Pv9B7lGUJnq_tTVx9UHFZx4GpbAvtwjSUIpFfiDoRZU'
+
 async function run() {
 	try {
 		// const user = await getUser(1);
 		// const prompts = await allPrompts();
 		const newUser = await createUser(userData);
 		console.log(newUser);
+		// const { token } = newUser;
+		// console.log('token: ', token);
+		// const user = await getUser(5, token);
+		// console.log(user);
 	} catch (e) {
 		console.log(e);
-	}
+	};
 }
 run();
