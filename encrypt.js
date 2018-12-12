7fb7a479960146e9f8ec11711fb3da920ffc7e5a
Curt Morgan <curtmorgan@ool-ad0205a4.dyn.optonline.net>
2018-12-12 11:22:31 -0500

    new password and encrypt files

--- encrypt.js
+++ encrypt.js
@@ -1,12 +1,10 @@
-const bcrypt = require('bcrypt');
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
 const jwt = require('jsonwebtoken');
-const {User} = require('./models.js');
+const { User } = require('./models.js');
 
 const SECRET = "please don't tell";
-const SALT = 10;
 const sign = payload => jwt.sign(payload, SECRET);
 
 const opts = {
@@ -16,8 +14,6 @@ const opts = {
 
 passport.use(new JwtStrategy(opts, async (payload, done) => {
 	try {
-		console.log('encrypt, ', payload.id);
-		debugger;
 		const user = await User.findByPk(payload.id);
 		return done(null, user);
 	} catch (e) {
@@ -25,21 +21,7 @@ passport.use(new JwtStrategy(opts, async (payload, done) => {
 	}
 }));
 
-
-// Password hashing functions
-async function createHash(password) {
-	const passwordDigest = await bcrypt.hash(password, SALT);
-	return passwordDigest;
-}
-
-async function compare(password, hashedPassword) {
-	const isValid = bcrypt.compare(password, hashedPassword);
-	return isValid;
-}
-
 module.exports = {
-	createHash,
-	compare,
 	sign,
 	passport,
 };
