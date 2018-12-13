a89e274d1a4a699d52292e11c428cd0380e9383f
Curt Morgan <curtmorgan@ool-ad0205a4.dyn.optonline.net>
2018-12-13 11:51:59 -0500

    Stash changes

--- client/src/components/ViewUserContributions/index.js
+++ client/src/components/ViewUserContributions/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ViewCompletedStories from '../ViewCompletedStories';
 import { getCurrentUser, allUserPrompts, allUserChapters, allUserStories } from '../../ajax-helpers.js';
 
 export default class ViewUserContributions extends React.Component{
@@ -34,7 +35,7 @@ export default class ViewUserContributions extends React.Component{
 	render(){
 		return(
 			<div>
-				<h1>Contributions</h1>
+				<ViewCompletedStories stories={this.state.stories} />
 			</div>
 		)
 	}
