16bc7ac897c885a312b51696959a9eeededa29d0
EileenMcCarren <emccarren@yahoo.com>
2018-12-13 19:28:51 -0500

    logo added to welcome

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logoimage from './wireframe1.JPG';
+import logoimage from './Images/AdobeStock_113970404.svg';
 
 export default () => (
   <div className='logo'>
-    <img src={logoimage} alt='logo' width='100px' height='100px'/>
+    <img src= {logoimage} alt='logo' width='100px' height='100px'/>
   </div>
 )
