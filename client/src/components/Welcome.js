395f7f748cc8a4b840647b005dcda93a3e7ebaf6
EileenMcCarren <emccarren@yahoo.com>
2018-12-13 20:42:33 -0500

    wlcome page div style and margins for div set up

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -5,7 +5,8 @@ import Logo from  './Logo';
 export default (props) => (
   <div className='welcome-container'>
     <Logo />
-    <h1 className='title'>Welcome to Exquisite Chronicle</h1>
+    <h1 className='title'>WELCOME TO</h1>
+    <h2 className='second-line'>Exquisite Chronicle</h2>
     <p className='welcome-text'>A collaborative writing application based off of the idea of Exquisite Corpse,
       where several people register to participate in the genre of a particular category of narrative and
       share in the surprise of the cumulative story.</p>
